ac172b548613864056f8ab2b737689b432a20b25
grassclipper <dragonstad@gmail.com>
2014-11-19 19:34:33 -0500

    made manual arm raiser that works

---
+++ library/display.h
@@ -0,0 +1,12 @@
+#pragma systemfile
+//Constants for Screen coordinates
+const int LEFT_X = 3;
+const int RIGHT_X = 85;
+const int BOTTOM_Y = 14;
+const int TOP_Y = 60;
+//Constants for button definitions
+const int NO_BUTTON = -1;
+const int RIGHT_BUTTON = 1;
+const int LEFT_BUTTON = 2;
+const int ORANGE_BUTTON = 3;
+const int DARK_BUTTON = 0;
