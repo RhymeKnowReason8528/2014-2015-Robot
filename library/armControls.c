869940a1974b48e26083db915b07b0241a8a3e5d
grassclipper <dragonstad@gmail.com>
2014-12-15 14:58:46 -0500

    simplified if/else controlling arm

--- library/armControls.c
+++ library/armControls.c
@@ -5,13 +5,11 @@ const int ARM_MIDDLE = 0;
 const int ARM_EXTENDED = 1;
 
 int getArmPosition(){
-	if(armInternal == 1){
-		if(armExternal == 1){
-			return ARM_FOLDED;
-		}
-		else{
-			return ARM_EXTENDED;
-		}
+	if(armExternal == 1){
+		return ARM_FOLDED;
+	}
+	else if(armInternal == 1){
+		return ARM_EXTENDED;
 	}
 	else{
 		return ARM_MIDDLE;
@@ -20,7 +18,7 @@ int getArmPosition(){
 
 void extendArm(){//ONLY USE IN AUTONOMOUS
 	motor[arm] = 100;//starts raising
-	while(sensorValue(armInternal) == 0)//waits in loop until arm is extended
+	while(SensorValue(armInternal) == 0)//waits in loop until arm is extended
 	{}
 	motor[arm] = 0;
 }
