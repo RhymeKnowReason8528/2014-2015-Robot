8d851407aa27f391433777e73ed0c07dcabd5f7c
grassclipper <grassclipper@moeftc.org>
2015-01-24 16:16:45 -0500

    added chooseWait function

--- library/IRramp.c
+++ library/IRramp.c
@@ -2,6 +2,7 @@
 
 #include "C:\Users\Emma\Documents\GitHub\robotcdriversuite\include\hitechnic-irseeker-v2.h"
 #include "driveFunctions.c"
+#include "library\display.h"
 
 int IRrampScan1 ()
 {
@@ -20,4 +21,33 @@ int IRrampScan1 ()
 	displayTextLine(2, "First Reading %d", read1);
 	displayTextLine(5, "Second reading %d", read2);
 	return 0;
+}
+
+int chooseWait ()
+{
+	int howLong = 0;
+	while(nNxtButtonPressed != ORANGE_BUTTON)
+	{
+		wait1Msec(100);
+		eraseDisplay();
+		displayStringAt(LEFT_X, BOTTOM_Y, "Less");
+		displayStringAt(RIGHT_X - 10, BOTTOM_Y, "More");
+		displayCenteredTextLine(2, "Wait %d", howLong);
+		switch((int)nNxtButtonPressed){
+		case LEFT_BUTTON://if the left button is pressed to raise arm
+		while(nNxtButtonPressed == LEFT_BUTTON)
+		{
+		}
+			howLong -= 1000;
+			break;
+		case RIGHT_BUTTON://if the right button is pressed to lower the arm
+		while(nNxtButtonPressed == RIGHT_BUTTON)
+		{
+		}
+			howLong += 1000;
+			break;
+		}
+	}
+	eraseDisplay();
+	return howLong;
 }
